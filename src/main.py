7282145c6af0939923f40494565f54a5dec38519
AndreyBarreto <andreyvieirabarreto@hotmail.com>
2021-11-04 16:39:23 -0300

    feature/middleware jwt

--- src/main.py
+++ src/main.py
@@ -3,6 +3,9 @@ from flask_sqlalchemy import SQLAlchemy
 from dotenv import load_dotenv
 import os
 from models.models import Models
+from functools import wraps
+import jwt
+
 
 #####Services######
 from services.personServices.createPersonService import createPersonService
@@ -20,7 +23,6 @@ CreateUser = createUserService()
 load_dotenv()
 
 app = Flask(__name__)
-app.config['JWT_SECRET_KEY'] = 'secret-secret2'
 
 app.config['SQLALCHEMY_DATABASE_URI'] = f"postgresql+psycopg2://{os.environ['POSTGRES_USER']}:{os.environ['POSTGRES_PASSWORD']}@{os.environ['POSTGRES_HOST']}/{os.environ['POSTGRES_NAME']}"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -29,12 +31,34 @@ db = SQLAlchemy(app)
 (PeoplesModel, CarsModel, UsersModel) = Models.generate(db)
 
 
+def token_required(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        token = None
+        if 'HTTP_X_ACESS_TOKEN' in request.headers.environ:
+            token = request.headers.environ['HTTP_X_ACESS_TOKEN']
+        if not token:
+            return {'error': 'Token is missing'}, 401
+
+        try:
+            jwt.decode(
+                token, os.environ['SECRET_KEY'], algorithms=["HS256"])
+        except:
+            return {
+                'error': 'Token is invalid'
+            }, 401
+        return f(*args, **kwargs)
+
+    return decorated
+
+
 @app.route("/")
 def home():
     return {"hello": "CarFord Town"}
 
 
 @app.route("/people", methods=["POST"])
+@token_required
 def create_person():
     data = request.json
     isNotValidJson = CreatePerson.validate(data)
@@ -45,6 +69,7 @@ def create_person():
 
 
 @app.route("/car/<id_people>", methods=["POST"])
+@token_required
 def create_car(id_people):
     data = request.json
     isNotValidJson = CreateCar.validate(data)
@@ -55,6 +80,7 @@ def create_car(id_people):
 
 
 @app.route("/register", methods=["POST"])
+@token_required
 def register_user():
     data = request.json
     isNotValidJson = CreateUser.validate(data, db, UsersModel)
--- src/services/userServices/createUserService.py
+++ src/services/userServices/createUserService.py
@@ -1,8 +1,12 @@
+from dotenv.main import load_dotenv
 from domains.userDomain import UserDomain
 from jsonschema import validate
 from werkzeug.security import generate_password_hash, check_password_hash
 import jwt
 from datetime import datetime, timedelta
+import os
+
+load_dotenv()
 
 
 class createUserService:
@@ -35,14 +39,17 @@ class createUserService:
 
     def create(self):
         userAlreadyExist = UserDomain.find(self.data, self.db, self.UsersModel)
+
         if userAlreadyExist:
             return {"error": "Username already exists"}, 400
+
         self.data['password'] = generate_password_hash(self.data['password'])
 
         return UserDomain.create(self.data, self.db, self.UsersModel)
 
     def verifyPassword(self):
         user = UserDomain.find(self.data, self.db, self.UsersModel)
+
         if not user:
             return {"error": "invalid username or password"}, 400
 
@@ -50,7 +57,8 @@ class createUserService:
             token = jwt.encode({
                 'public_id': user.id,
                 'exp': datetime.utcnow() + timedelta(minutes=30)
-            }, "randomdsasad")
+            }, os.environ['SECRET_KEY'])
+
             return {"token": token}
 
         else:
